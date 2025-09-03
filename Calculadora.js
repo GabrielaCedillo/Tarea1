7b4ff7dc461a4752074756a51f1856bc8552c56b
GabrielaCedillo <gabychubis.cf@gmail.com>
2025-09-02 22:26:07 -0600

    Tarea 1 Calculadora

---
+++ Calculadora.js
@@ -0,0 +1,7 @@
+var numero1 = parseFloat(prompt("Numero 1"));
+var numero2 = parseFloat(prompt("Numero 2"));
+
+console.log("Suma :"+ (numero1+numero2))
+console.log("Resta :"+ (numero1-numero2))
+console.log("Multiplicacion :"+ (numero1*numero2))
+console.log("Division :"+ (numero1/numero2))
