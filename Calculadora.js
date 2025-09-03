bc9ca9bb0cf25d37a321403750e72af77dff9f97
GabrielaCedillo <gabychubis.cf@gmail.com>
2025-09-02 22:30:29 -0600

    comentarios del funcionamiento

--- Calculadora.js
+++ Calculadora.js
@@ -1,7 +1,7 @@
-var numero1 = parseFloat(prompt("Numero 1"));
+var numero1 = parseFloat(prompt("Numero 1"));//aparece el prompt para que el usuario ingrese el numero 1
 var numero2 = parseFloat(prompt("Numero 2"));
 
-console.log("Suma :"+ (numero1+numero2))
-console.log("Resta :"+ (numero1-numero2))
-console.log("Multiplicacion :"+ (numero1*numero2))
-console.log("Division :"+ (numero1/numero2))
+console.log("Suma :"+ (numero1+numero2))// en la consola se imprime la suma de los dos numero proporcionados por el ususario
+console.log("Resta :"+ (numero1-numero2))// resta
+console.log("Multiplicacion :"+ (numero1*numero2)) //multiplicación
+console.log("Division :"+ (numero1/numero2)) // division 
